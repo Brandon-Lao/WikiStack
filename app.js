26bccc332dc526b1040debee4396d44a8caedf6b
Brandon Lao <brandon.lao11@gmail.com>
2018-06-15 12:02:27 -0400

    Starting sync

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const morgan = require("morgan");
+// const client = require("./db");
+const layout = require("./views/layout.js");
+const bodyParser = require('body-parser');
+const { db } = require('./models');
 
 const app = express();
 
+
 app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+// app.use('/posts', routes);
 
 app.get("/", (req, res) => {
-  console.log("Test");
+  res.send(layout(''));
 });
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
+});
+
+db.authenticate().then(() => 
+{
+	console.log('Database connection online.');
 });
